06425edd2bfe5b766bf23bca923bfcb68d3a6c72
Pappy <DPapworth@academytrainee.com>
2021-06-01 14:48:40 +0000

    unit tests complete cov 100%

--- operator_random/app.py
+++ operator_random/app.py
@@ -15,5 +15,4 @@ def operator():
         dif = 20 
     return jsonify({"operator":op, "difficulty":dif})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__": app.run(host="0.0.0.0", port=5000, debug=True)
