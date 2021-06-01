926037b8d9b9ccf9cd547805ca236e9358c7849b
Pappy <DPapworth@academytrainee.com>
2021-06-01 16:26:49 +0000

    update test

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ pip3 install -r requirements.txt
 export DATABASE_URI
 export SECRET_KEY
 
-python3 -m pytest front_end --junitxml=junit/test-results.xml --cov=app --cov-report=xml --cov-report=html
+python3 -m pytest front-end --junitxml=junit/test-results.xml --cov=app --cov-report=xml --cov-report=html
 python3 -m pytest operator_random --junitxml=junit/test-results1.xml --cov=app --cov-report=xml --cov-report=html
 python3 -m pytest strat_random --junitxml=junit/test-results2.xml --cov=app --cov-report=xml --cov-report=html
 python3 -m pytest points --junitxml=junit/test-results3.xml --cov=app --cov-report=xml --cov-report=html
